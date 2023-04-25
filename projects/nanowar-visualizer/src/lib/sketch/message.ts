49df8d7b1d145fa1b3af134e982bb7edc3bc40de
Leitereg András <andras.leitereg@gmail.com>
2023-04-25 11:02:21 +0200

    Show bot log

--- projects/nanowar-visualizer/src/lib/sketch/message.ts
+++ projects/nanowar-visualizer/src/lib/sketch/message.ts
@@ -14,10 +14,12 @@ export class BotMessageBundle {
   public received: BotMessage[];
   public sent: BotMessage[];
   public error: BotMessage[];
+  public botLog: BotMessage[];
 
   constructor(json_bot_message: JsonBotMessage) {
     this.received = json_bot_message.received.map((x) => new BotMessage(x));
     this.sent = json_bot_message.sent.map((x) => new BotMessage(x));
     this.error = json_bot_message.commandError ? [new BotMessage({ message: json_bot_message.commandError, timestamp: null })] : [];
+    this.botLog = json_bot_message.botLog ? [new BotMessage({ message: json_bot_message.botLog, timestamp: null })] : [];
   }
 }
