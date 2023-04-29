4bd0668785c065a615073b388cc58745e206ac8d
radl97 <radl97@users.noreply.github.com>
2023-04-29 17:03:58 +0200

    Fixup

--- projects/nanowar-visualizer/src/lib/sketch/planet.ts
+++ projects/nanowar-visualizer/src/lib/sketch/planet.ts
@@ -39,7 +39,7 @@ export class Planet {
     }
     ctx.stroke('#ffffff');
     ctx.strokeWeight(1);
-    ctx.fill(this.player.color);
+    ctx.fill(color);
     ctx.textSize(12);
     ctx.text(this.population, this.pos.x, this.pos.y);
     ctx.textSize(6);
